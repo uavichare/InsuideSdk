656ba269de06344c4e12cae712e5af77695faa78
dilipgupta123-123 <GDIlip10#$%>
2022-09-12 22:02:36 +0530

    first commit

---
+++ DubaiLibrary/src/main/java/com/example/dubailibrary/MethodScreen.kt
@@ -0,0 +1,16 @@
+package com.example.dubailibrary
+
+import com.indooratlas.android.sdk.IAOrientationListener
+
+
+class MethodScreen:IAOrientationListener {
+    override fun onHeadingChanged(p0: Long, p1: Double) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onOrientationChange(p0: Long, p1: DoubleArray?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
