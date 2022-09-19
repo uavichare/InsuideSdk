5be883a40d2559bd9c510771615d1e6ded53a5ad
dilipgupta123-123 <GDIlip10#$%>
2022-09-19 23:20:50 +0530

    second commit

---
+++ DubaiLibrary/src/main/java/com/example/dubailibrary/InsuideSDK.kt
@@ -0,0 +1,28 @@
+package com.example.dubailibrary
+
+import android.content.Context
+import android.content.Intent
+import androidx.annotation.Keep
+
+class InsuideSDK {
+    // companion object SDKMethods {
+         fun authenticationInsuideIndoor(context: Context, apiKey: String, apiSecretKey: String) {
+         }
+
+         fun authenticationOfMapbox(context: Context, token: String) {
+         }
+
+         fun OpenSdkScreen(c: Context) {
+             var intent: Intent? = null
+             try {
+                 intent = Intent(c, Class.forName("com.example.buglibrary.SDKActivity"))
+                 c.startActivity(intent)
+             } catch (e: ClassNotFoundException) {
+                 e.printStackTrace()
+             }
+         }
+
+     //}
+
+
+}
